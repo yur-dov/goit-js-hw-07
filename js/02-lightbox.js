9ccdefd45b6af08633e129b2eb1db25d9adf56f2
Yurii <Rapacta@gmail.com>
2022-05-08 15:04:26 +0200

    2-task-complite

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,17 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imageBlock = document.querySelector('.gallery')
+
+const gallerySet = gallaryAdd(galleryItems)
+
+function gallaryAdd(elements) {
+const newArr = elements.map(item => `<a class="gallery__item" href="${item.original}">
+    <img class="gallery__image lazyload blur-up" data-src="${item.preview}" alt="${item.description}"  />
+</a>`).join('')
+
+    return newArr
+}
+
+imageBlock.innerHTML = gallerySet
+
+let gallery = new SimpleLightbox('.gallery a', {captionsData :'alt', captionDelay: 250});
